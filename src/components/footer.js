ff4126325a3cc589e6022f549b1ed2cf48ed050e
Funmilayo Akano <akanofunmilayo44@gmail.com>
2024-12-12 14:10:01 +0100

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
-import logo from "../BG-img/logo.png"
-import { Link } from "react-router-dom";
+// import logo from "../BG-img/logo.png"
+// import { Link } from "react-router-dom";
 
 const Footer = () => {
     return ( 
